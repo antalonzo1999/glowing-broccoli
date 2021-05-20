d637416709474749f4470249cb7c1a838b8658c6
antalonzo1999 <82597387+antalonzo1999@users.noreply.github.com>
2021-05-20 10:00:31 -0400

    final project link to  MMP100 files

---
+++ empty-example/sketch.js
@@ -0,0 +1,38 @@
+
+
+function setup() {
+  createCanvas(600, 600);
+  background("beige");
+  colorMode(HSB);
+}
+
+function draw() {
+  //Line from prev pt to current pt
+  //of mouse position
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
+
+//listen when we click the mouse
+function mouseClicked() {
+	//weight o to 1
+	stroke("slateblue");
+	strokeWeight(random());
+
+	// what if want weight o to 4?
+	//strokeWeight( random(.4) );
+}
+
+//listen when we release *any* key
+function keyReleased() {
+	//color hue values between 20 and 145
+	//saturation o to 100
+	//brightness 80 to 100
+	stroke(random(20, 145), random(100), random(80, 100));
+}
+
+//listen for only character keys
+function KeyTyped() {
+	//weights 0 to 5
+	stroke("turquoise");
+	strokeweight(random(5));
+}
